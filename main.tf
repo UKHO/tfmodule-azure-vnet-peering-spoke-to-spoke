354d205f1b6445ef7c9765b85767caa3ede5f548
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2020-02-20 11:24:55 +0000

    setting default

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_virtual_network_peering" "vnet_peer_1" {
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
   allow_gateway_transit        = false
-  use_remote_gateways          = true
+  use_remote_gateways          = false
 }
 
 resource "azurerm_virtual_network_peering" "vnet_peer_2" {
@@ -52,7 +52,7 @@ resource "azurerm_virtual_network_peering" "vnet_peer_2" {
   remote_virtual_network_id    = data.azurerm_virtual_network.onevnet.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
-  allow_gateway_transit        = true
+  allow_gateway_transit        = false
   use_remote_gateways          = false
 }
 
