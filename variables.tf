29504b9eb25473e3657906c3e5c79621f4755ba2
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2023-11-13 09:27:56 +0000

    Update readme (#2)
    * Update README.md
    
    * Remove defaults

--- variables.tf
+++ variables.tf
@@ -1,20 +1,14 @@
 variable "subscription_one_rg" {
-  default = ""
 }
 variable "subscription_two_rg" {
-  default = ""
 }
 variable "subscription_one_vnet" {
-  default = ""
 }
 variable "subscription_two_vnet" {
-  default = ""
 }
 variable "peer_one_to_two" {
-  default = ""
 }
 variable "peer_two_to_one" {
-  default = ""
 }
 
 
